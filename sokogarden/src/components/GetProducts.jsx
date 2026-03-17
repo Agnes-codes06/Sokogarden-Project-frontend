b6ff9dce9e289b710ca7af5bb3fe9cbf1d5efeba
Agnes-codes06 <wagnes211@gmail.com>
2026-03-17 13:36:20 +0300

    added the getproduct component with all the code and api to fetch product from the database

--- sokogarden/src/components/GetProducts.jsx
+++ sokogarden/src/components/GetProducts.jsx
@@ -1,12 +1,84 @@
 import React from 'react'
-
+import { useState,useEffect} from "react"; 
+import axios from "axios"; 
 const GetProducts = () => {
+
+  const[loading,setLoading]=useState("")
+  const[error,setError]=useState("")
+  const[products,setProducts]=useState([])
+
+  // variable to store image
+  const image_url="https://agnes.alwaysdata.net/static/images/"
+
+
+  // create a function to get our products from the api
+
+  const fetchProducts=async()=>{
+
+    setLoading("Please wait as we retrieve your products")
+
+    try {
+
+      const response=await axios.get("https://agnes.alwaysdata.net/api/getproductdetails")
+
+      console.log("the response is",response)
+
+      setProducts(response.data)
+      setLoading("")
+      
+    } catch (error) {
+
+      setLoading("")
+      setError(error.message)
+      
+    }
+
+  }
+
+
+  // end of function to call the use effect
+
+  useEffect(()=>{
+    fetchProducts()
+  },[])
+
+
   return (
-    <div>
+    <div className='row'>
+      <h1>Available Products</h1>
 
-        <h1>Welcome to Getproducts component</h1>
+      <p className='text-warning'>{loading}</p>
+      <p className='text-danger'>{error}</p>
+
+      {/* loop through our product */}
+      {products.map((product)=>(
+
+      
+      <div className='col-md-3 justify-content-center'>
+
+        <div className='card shadow m-2'>
+
+        <img src={image_url + product.product_photo} alt="cake" className='product_img mt-4' />
+
+        <div className='card-body'>
+
+          <h4 className='text-success'>{product.product_name}</h4>
+          <p className='text-secondary'>{product.product_description}</p>
+          <p className='text-warning'>Ksh:{product.product_cost}</p>
+
+          <input type="button" value="Make Payments" className='btn btn-secondary w-100' />
 
 
+        </div>
+
+
+      </div>
+
+      </div>
+
+      ))}
+
+      
     </div>
   )
 }
